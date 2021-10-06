9a5392076c2262ff285c6161224bc20db78881ea
miloje-djukanovic <mr21050@alas.matf.bg.ac.rs>
2021-10-06 13:25:46 +0200

    layout change

--- script.js
+++ script.js
@@ -4,19 +4,24 @@ on_title_click = () =>
 {
 	let content = event.target.parentNode.querySelector( ".subject_content" );
 
-	if( content.style.display !== "block" )
-	{
-		content.style.display = "block"
-	}
-	else
-	{
-		content.style.display = "none"
-	}
-
-	event.target.classList.toggle( "clicked" );
+	content.classList.toggle( "invisible" )
+	event.target.classList.toggle( "clicked" )
 }
 
 for( let i = 0; i < title.length; i ++ )
 {
-	title[ i ].addEventListener( "click", on_title_click );
+	title[ i ].addEventListener( "click", on_title_click )
 }
+
+let subs_head = document.querySelector( "#subjects_head" )
+
+on_subs_head_click = () =>
+{
+	console.log( "sub head click" )
+	let subs = event.target.parentNode.querySelector( ".subjects" )
+
+	console.log( subs )
+	subs.classList.toggle( "invisible" )
+}
+
+subs_head.addEventListener( "click", on_subs_head_click )
